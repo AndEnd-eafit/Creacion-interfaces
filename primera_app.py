0fe2e6e0db760bf6b2c3f5ce09d2fedd0df9bb3e
AndEnd-eafit <aprivasm@eafit.edu.co>
2024-08-15 07:29:25 -0500

    Update primera_app.py

--- primera_app.py
+++ primera_app.py
@@ -8,3 +8,17 @@ st.write("Facilmente puedo realizar backend y frontend")
 image = Image.open('Volvi.png')
 
 st.image(image, caption='Mini Yoru')
+
+texto = st.text_input('Venga, escribe algo, lo que se te de la gana', 'Observe')
+st.write('EL texto escrito es', texto)
+
+st.subheader("Ahora usemos dos columnas")
+
+col1, col2 = st.columns(2)
+
+with col1:
+  st.subheader("Esta es la primera columna")
+  st.write("Las interfaces multimodales mejoran la experiencia del usuario")
+  resp = st.checkbox('Estoy de acuerdo')
+  if resp:
+    st.write('Yesssssssss')
