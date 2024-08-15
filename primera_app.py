3ca8eec75cfabe9311609c0525e2a75c193d99c9
AndEnd-eafit <aprivasm@eafit.edu.co>
2024-08-15 07:15:17 -0500

    Update primera_app.py

--- primera_app.py
+++ primera_app.py
@@ -5,3 +5,6 @@ st.title(" Primer ejercicio")
 
 st.header("en este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales")
 st.write("Facilmente puedo realizar backend y frontend")
+image = Image.open('Volvi.png')
+
+st.image(image, caption='Mini Yoru')
