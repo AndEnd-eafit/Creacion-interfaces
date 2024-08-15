f157093b43ffcc6a5e3eb36655417b578a4735d7
AndEnd-eafit <aprivasm@eafit.edu.co>
2024-08-15 07:10:48 -0500

    Update primera_app.py

--- primera_app.py
+++ primera_app.py
@@ -1,1 +1,7 @@
+import streamlit as st
+from PIL import Image
 
+st.title(" Primer ejercicio")
+
+st.header("en este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales")
+st.write("Facilmente puedo realizar backend y frontend")
